c29249cf09d94bd8405b679d2fe5934abcb2507d
MarcosVinicius <marcusvinycius73@gmail.com>
2024-02-19 21:24:39 -0300

    feat: add background image

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,10 +33,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
+                    GreetingImages(
                         message = "Happy birthday, Marcos",
                         from = "From: Camila",
-                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -67,27 +68,21 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingImages(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty__1_)
-
-    Image(
-        painter = image,
-        contentDescription = null
-    )
-
-    Text(
-        text = message,
-        fontSize = 80.sp,
-        lineHeight = 100.sp,
-        textAlign = TextAlign.Center
-    )
-
-    Text(
-        text = from,
-        fontSize = 36.sp,
-        modifier = Modifier
-            .padding(16.dp)
-
-    )
-
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 }
 
 
